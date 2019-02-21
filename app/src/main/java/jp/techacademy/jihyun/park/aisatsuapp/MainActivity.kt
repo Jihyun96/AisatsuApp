3c237f5d04c6d18fd7dec5228955eba3c943d7e5
jihyun <ji_hyun96@naver.com>
2019-02-21 23:21:17 +0900

    挨拶アプリ

--- app/src/main/java/jp/techacademy/jihyun/park/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/jihyun/park/aisatsuapp/MainActivity.kt
@@ -2,11 +2,36 @@ package jp.techacademy.jihyun.park.aisatsuapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.app.TimePickerDialog
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View){
+        showTimePickerDialog()
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                if (2 <= hour && hour < 10) {
+                    textView.text = "おはよう"
+                } else if (10 <= hour && hour < 18) {
+                    textView.text = "こんにちは"
+                } else {
+                    textView.text = "こんばんは"
+                }
+            },
+            13,0,true)
+        timePickerDialog.show()
     }
 }
